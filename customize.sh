36a6d37b82ae0eab9de640b8117a7f02c34e16a7
djh5222 <78486895+djh5222@users.noreply.github.com>
2021-05-02 22:06:54 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,8 +2,8 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: eSirPlayground
+# Author: FireDJ
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/10.0.10.1/10.0.10.2/g' openwrt/package/base-files/files/bin/config_generate
